b71c1ddf1e9ced462a331aa13086f7509ed822d3
pathel <polegend@stud.ntnu.no>
2021-03-19 11:15:40 +0100

    added to pca

--- src/pca.py
+++ src/pca.py
@@ -1,5 +1,13 @@
-
+import numpy as np
+import matplotlib.pyplot as plot
+import scipy as sp
 
 class PCA:
     def __init__(self):
+        pass
+
+    def fit(self):
+        pass
+    
+    def transform(self):
         pass
